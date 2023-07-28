1b3f8a236dda9d3d186fac1f51da4adc326acc54
David Coy <dcoy@gitlab.com>
2023-07-28 08:52:07 -0500

    Working as intended
    Signed-off-by: David Coy <dcoy@gitlab.com>

--- rw_a-backend/main.go
+++ rw_a-backend/main.go
@@ -1,68 +1,137 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"strings"
+
+	"net/url"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/v53/github"
+	"golang.org/x/oauth2"
 )
 
-func setupRouter() *gin.Engine {
-	gin.DisableConsoleColor()
+var (
+	accessToken = os.Getenv("GH_API_TOKEN")
+)
 
+type GitHubIssue struct {
+	Number   int    `json:"number"`
+	Title    string `json:"title"`
+	State    string `json:"state"`
+	IssueUrl string `json:"html_url"`
+}
+
+type UrlParts struct {
+	Scheme   string
+	Host     string
+	Path     string
+	RawQuery string
+}
+
+type GhRepoUri struct {
+	Owner     string
+	Repo      string
+	ExtraInfo []string
+}
+
+func parseUrl(repoUrl string) (*UrlParts, error) {
+	u, err := url.Parse(repoUrl)
+	if err != nil {
+		return nil, err
+	}
+	urlParts := &UrlParts{
+		Scheme:   u.Scheme,
+		Host:     u.Host,
+		Path:     u.Path,
+		RawQuery: u.RawQuery,
+	}
+	return urlParts, nil
+}
+
+func splitPath(path string) (*GhRepoUri, error) {
+	parts := strings.Split(path, "/")
+	parts = parts[1:]
+
+	if len(parts) < 2 {
+		return nil, nil
+	}
+
+	gitHubRepo := &GhRepoUri{
+		Owner: parts[0],
+		Repo:  parts[1],
+	}
+
+	if len(parts) > 2 {
+		gitHubRepo.ExtraInfo = parts[2:]
+	}
+
+	return gitHubRepo, nil
+}
+
+func listOpenIssues(c *gin.Context) {
+	repoUrl := c.PostForm("repo")
+	// parse repoUrl and split it
+	parsedUrl, _ := parseUrl(repoUrl)
+	splitUrl, _ := splitPath(parsedUrl.Path)
+
+	owner := splitUrl.Owner
+	repo := splitUrl.Repo
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	opt := &github.IssueListByRepoOptions{
+		State: "open",
+	}
+	issues, _, err := client.Issues.ListByRepo(ctx, owner, repo, opt)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", gin.H{"Error": err.Error()})
+		return
+	}
+
+	var openIssues []GitHubIssue
+	for _, issue := range issues {
+		openIssues = append(openIssues, GitHubIssue{
+			Number:   issue.GetNumber(),
+			Title:    issue.GetTitle(),
+			State:    issue.GetState(),
+			IssueUrl: issue.GetHTMLURL(),
+		})
+	}
+
+	c.HTML(http.StatusOK, "issues.html", gin.H{
+		"Owner":    owner,
+		"Repo":     repo,
+		"Issues":   openIssues,
+		"Number":   openIssues,
+		"IssueUrl": openIssues,
+	})
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-
-	// client := github.NewClient(nil)
-	// 	token := os.Getenv("GITHUB_API_TOKEN")
-
-	// issues := client.
-
-	// Ping test
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"path": "path", "value": "pong"})
-	})
-
-	router.GET("/api", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"key": "key", "value": "value"})
-	})
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(
 			http.StatusOK,
 			"index.html",
 			gin.H{
-				"title": "GH Issues Renderer",
+				"title": "RW GH Issues Renderer",
 			},
 		)
 	})
+	router.POST("/issues", listOpenIssues)
 
-	// Get user value
-	// r.GET("/user/:name", func(c *gin.Context) {
-	// 	user := c.Params.ByName("name")
-	// 	value, ok := db[user]
-	// 	if ok {
-	// 		c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-	// 	} else {
-	// 		c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-	// 	}
-	// })
+	port := "8080"
 
-	// // Authorized group (uses gin.BasicAuth() middleware)
-	// // Same than:
-	// // authorized := r.Group("/")
-	// // authorized.Use(gin.BasicAuth(gin.Credentials{
-	// //	  "foo":  "bar",
-	// //	  "manu": "123",
-	// //}))
-	// authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-	// 	"foo":  "bar", // user:foo password:bar
-	// 	"manu": "123", // user:manu password:123
-	// }))
-
-	return router
-}
-
-func main() {
-	router := setupRouter()
-	router.Run(":8080")
+	router.Run(":" + port)
 }
